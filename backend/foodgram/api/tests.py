ba7949281ac06c6f210d3cd98c6474cef69670d4
Timur Bazhikov <t.bazhikov@gmail.com>
2025-05-23 00:05:23 +0300

    delete import of reverse

--- backend/foodgram/api/tests.py
+++ backend/foodgram/api/tests.py
@@ -1,6 +1,5 @@
 from http import HTTPStatus
 from django.test import Client, TestCase
-from django.urls import reverse
 from django.contrib.auth import get_user_model
 
 from recipes.models import Recipe, Ingredient, Favourite, RecipeIngredientValue
